f347d72b40db3322996bf5841213e5290b6050d4
sennerstampontus <sennerstam.pontus@gmail.com>
2021-11-03 11:17:33 +0100

    View Customer done

--- src/components/CustomerList.js
+++ src/components/CustomerList.js
@@ -1,21 +1,20 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import Customer from './Customer'
 
 const CustomerList = () => {
 
-    const [customerItem, setCustomer] = useState([
-    {
-        firstName: "Pontus",
-        lastName: "Sennerstam",
-        email: "sennerstam.pontus@gmail.com"
-            
-    }
-    ])
+    const [customerItem, setCustomer] = useState([])
+
+    useEffect(()=>{
+        fetch("https://ecexam-webapi.azurewebsites.net/api/Customers")
+        .then(res => res.json())
+        .then(data => setCustomer(data))
+    },[])
 
     return (
         <ul className="list-group">
             {
-                customerItem.map(customerItem =><Customer cardTitle={customerItem.firstName + " " + customerItem.lastName} cardDesc={customerItem.email} />)
+                customerItem.map(customerItem =><Customer key={customerItem.id} cardTitle={customerItem.firstName + " " + customerItem.lastName} cardDesc={customerItem.email} />)
             }
             
         </ul>
